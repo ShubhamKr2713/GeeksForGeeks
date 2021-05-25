a686cdf1b69f87d6e9892e8750664c7d1fd2ebc3
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-05-25 18:26:18 +0530

    Max and Second Max
    Max and Second Max 
    Easy Accuracy: 49.94% Submissions: 30208 Points: 2
    Given an array arr[] of size N of positive integers which may have duplicates. The task is to find the maximum and second maximum from the array, and both of them should be distinct, so If no second max exists, then the second max will be -1.
    
    Example 1:
    
    Input:
    N = 3
    arr[] = {2,1,2}
    Output: 2 1
    Explanation: From the given array 
    elements, 2 is the largest and 1 
    is the second largest.
    Example 2:
    
    Input:
    N = 5
    arr[] = {1,2,3,4,5}
    Output: 5 4
    Explanation: From the given array 
    elements, 5 is the largest and 4 
    is the second largest.
    Your Task:
    The task is to complete the function largestAndSecondLargest(), which should return maximum and second maximum element from the array with first element as maximum element and second element as second maximum(if there is no second maximum the  second element should be -1)
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(1).
    
    Constraints:
    1 <= N <= 106
    1 <= arr[i] <= 106

---
+++ Max and Second Max.cpp
@@ -0,0 +1,76 @@
+//Initial Template for C++
+
+// CPP code to find largest and 
+// second largest element in the array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+     
+
+class Solution{
+  public:
+    /* Function to find largest and second largest element
+    *sizeOfArray : number of elements in the array
+    * arr = input array
+    */
+    vector<int> largestAndSecondLargest(int n, int arr[]){
+        int maxx=*max_element(arr,arr+n);
+        int temp=0;
+        int max2=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<maxx)
+            {
+                temp=arr[i];
+                max2=max(max2,temp);
+            }
+        }
+        vector<int > ans;
+        ans.push_back(maxx);
+        if(temp==0)
+        ans.push_back(-1);
+        else
+        ans.push_back(max2);
+        return ans;
+        /*********************************
+         * Your code here
+         * This function should return a
+         * vector with first element as
+         * max and second element as 
+         * second max
+         * *******************************/
+         
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	
+	// Testcase input
+	int testcases;
+	cin >> testcases;
+    
+    // Looping through all testcases
+	while(testcases--){
+	    int sizeOfArray;
+	    cin >> sizeOfArray;
+	    
+	    int arr[sizeOfArray];
+	    
+	    // Array input
+	    for(int index = 0; index < sizeOfArray; index++){
+	        cin >> arr[index];
+	    }
+	    Solution obj;
+	    vector<int> ans = obj.largestAndSecondLargest(sizeOfArray, arr);
+	    cout<<ans[0]<<' '<<ans[1]<<endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends
