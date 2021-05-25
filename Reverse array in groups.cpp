450bb213899cddcae7df237a75e74e3155310771
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-05-25 19:09:58 +0530

    Reverse array in groups
    Reverse array in groups 
    Basic Accuracy: 51.16% Submissions: 40860 Points: 1
    Given an array arr[] of positive integers of size N. Reverse every sub-array group of size K.
    
     
    
    Example 1:
    
    Input:
    N = 5, K = 3
    arr[] = {1,2,3,4,5}
    Output: 3 2 1 5 4
    Explanation: First group consists of elements
    1, 2, 3. Second group consists of 4,5.
     
    
    Example 2:
    
    Input:
    N = 4, K = 3
    arr[] = {5,6,8,9}
    Output: 8 6 5 9
     
    
    Your Task:
    You don't need to read input or print anything. The task is to complete the function reverseInGroups() which takes the array, N and K as input parameters and modifies the array in-place. 
    
     
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(N)
    
     
    
    Constraints:
    1 ≤ N, K ≤ 107
    1 ≤ A[i] ≤ 1018

---
+++ Reverse array in groups.cpp
@@ -0,0 +1,57 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    //Function to reverse every sub-array group of size k.
+    void reverseInGroups(vector<long long>& arr, int n, int k){
+        // code here
+        if(k>n)
+        {
+            reverse(arr.begin(),arr.end());
+            return;
+        }
+        int i;
+        for(i=0;i<n-(n%k);i+=k)
+        {
+            auto it=arr.begin()+i;
+            reverse(it,it+k);
+        }
+        reverse(arr.begin()+i,arr.end());
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t; 
+    cin >> t; 
+    while(t--){ 
+        int n;
+        cin >> n; 
+        vector<long long> arr; 
+        int k;
+        cin >> k; 
+
+        for(long long i = 0; i<n; i++)
+        {
+            long long x;
+            cin >> x; 
+            arr.push_back(x); 
+        }
+        Solution ob;
+        ob.reverseInGroups(arr, n, k);
+        
+        for(long long i = 0; i<n; i++){
+            cout << arr[i] << " "; 
+        }
+        cout << endl;
+    }
+}
+
+  // } Driver Code Ends
