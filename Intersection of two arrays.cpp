b353c6f179b5be3e21f279319f234aa7747fd295
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-06-02 16:42:29 +0530

    Intersection of two arrays
    Intersection of two arrays 
    Easy Accuracy: 45.86% Submissions: 25210 Points: 2
    Given two arrays a[] and b[] respectively of size n and m, the task is to print the count of elements in the intersection (or common elements) of the two arrays.
    
    For this question, the intersection of two arrays can be defined as the set containing distinct common elements between the two arrays. 
    
    Example 1:
    
    Input:
    n = 5, m = 3
    a[] = {89, 24, 75, 11, 23}
    b[] = {89, 2, 4}
    
    Output: 1
    
    Explanation: 
    89 is the only element 
    in the intersection of two arrays.
    Example 2:
    
    Input:
    n = 6, m = 5
    a[] = {1, 2, 3, 4, 5, 6}
    b[] = {3, 4, 5, 6, 7} 
    
    Output: 4
    
    Explanation: 
    3 4 5 and 6 are the elements 
    in the intersection of two arrays.
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function NumberofElementsInIntersection() which takes two integers n and m and their respective arrays a[] and b[]  as input. The function should return the count of the number of elements in the intersection.
    
     
    
    Expected Time Complexity: O(n + m).
    Expected Auxiliary Space: O(min(n,m)).
    
    Constraints:
    1 ≤ n, m ≤ 105
    1 ≤ a[i], b[i] ≤ 105

---
+++ Intersection of two arrays.cpp
@@ -0,0 +1,49 @@
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    //Function to return the count of the number of elements in
+    //the intersection of two arrays.
+    int NumberofElementsInIntersection (int a[], int b[], int n, int m )
+    {
+        unordered_set<int> res(a,a+n);
+        int ans=0;
+        for(int i=0;i<m;i++)
+        {
+            if(res.count(b[i])==1)
+            {
+                ans++;
+                res.erase(b[i]);
+            }
+        }
+        return ans;
+        // Your code goes here
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,m;
+		cin>>n>>m;
+		int a[n], b[m];
+		for(int i=0; i<n; i++)
+			cin>>a[i];
+
+		for(int i=0; i<m; i++)
+			cin>>b[i];
+		Solution ob;
+		cout << ob.NumberofElementsInIntersection(a,b,n,m) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
