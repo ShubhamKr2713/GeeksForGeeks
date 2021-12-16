4a87df32a9ce5db3384f2750c7284de01383bd6d
Shubham Kumar <62713187+Shubham2713@users.noreply.github.com>
2021-12-16 23:15:04 +0530

    Pairwise swap of nodes in LinkedList
    Pairwise swap of nodes in LinkedList 
    Easy Accuracy: 70.79% Submissions: 9681 Points: 2
    Given a linked list of N positive integers. You need to swap elements pairwise. Your task is to complete the function pairwise_swap().
    
    Example 1:
    
    Input:
    N = 7
    value[] = {1,2,3,4,5,6,7}
    Output: 2 1 4 3 6 5 7
    Explanation:The linked list after swapping
    becomes: 1 2 3 4 5 6 7 -> 2 1 4 3 6 5 7.
    Example 2:
    
    Input:
    N = 6
    value[] = {1,2,3,4,5,6}
    Output: 2 1 4 3 6 5
    Explanation:The linked list after swapping
    becomes: 1 2 3 4 5 6 -> 2 1 4 3 6 5.
    Your Task:
    You don't have to worry about input and output, you just have to complete the function pairwise_swap(). The printing is done automatically by the driver code.
    
    Constraint:
    1 <= N <= 100
    1 <= Node values <= 1000
    
    Expected Time Complexity : O(N)
    Expected Auxilliary Space : O(1)

---
+++ Pairwise swap of nodes in LinkedList.cpp
@@ -0,0 +1,79 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+ // } Driver Code Ends
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to swap elements pairwise.
+struct Node* pairwise_swap(struct Node* head)
+{
+    // your code here
+    Node *first,*second;
+    first=head;
+    second=head->next;
+    while(first!=NULL && second!=NULL)
+    {
+        swap(first->data,second->data);
+        first=second->next;
+        if(second->next)
+        second=second->next->next;
+    }
+    return head;
+}
+
+// { Driver Code Starts.
+void printList(struct Node* node) 
+{ 
+    while (node!=NULL) 
+    { 
+        printf("%d ", (node)->data); 
+        node = (node)->next; 
+    } 
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int n, l,firstdata;
+        cin>>n;
+        // taking first node of linked list
+        cin>>firstdata;
+        struct Node* head = new Node(firstdata);
+        struct Node* tail = head;
+        // taking remaining nodes of linked list
+        for(int i = 1; i < n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        head =pairwise_swap(head);
+        printList(head);
+        cout << endl;
+    }
+}  // } Driver Code Ends
